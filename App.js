d8b2ee9a1deb256681849441377d97d4cfc8b276
jvsobraz <jvsobraz@hotmail.com>
2023-04-08 23:56:37 -0300

    Configuração do Multer para uploads de arquivos: Você pode configurar o Multer para lidar com uploads de arquivos para o Amazon S3:

--- App.js
+++ App.js
@@ -9,3 +9,18 @@ const s3 = new AWS.S3({
   secretAccessKey: 'YOUR_SECRET_ACCESS_KEY', // substitua com sua Secret Access Key do Amazon S3
   region: 'us-east-1' // substitua com a região do seu bucket do Amazon S3
 });
+
+const upload = multer({
+    storage: multerS3({
+      s3: s3,
+      bucket: 'YOUR_BUCKET_NAME', // substitua com o nome do seu bucket do Amazon S3
+      acl: 'public-read', // define as permissões de acesso aos arquivos
+      metadata: function (req, file, cb) {
+        cb(null, { fieldName: file.fieldname });
+      },
+      key: function (req, file, cb) {
+        cb(null, Date.now().toString() + '-' + file.originalname);
+      }
+    })
+  });
+  
